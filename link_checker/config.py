75750e39d2fcc682170586d3dd8279ea04ef8fac
Dmitry Golubkov <master@hsdesign.ru>
2019-11-12 01:57:36 +0300

    Minor PyDoc

--- link_checker/config.py
+++ link_checker/config.py
@@ -1,4 +1,6 @@
 import peewee
 
+# Turn off journaling temporary to prevent 'peewee.OperationalError: database is locked'
+# error in `web-access.py`.
 db = peewee.SqliteDatabase("history.sqlite", pragmas={"journal_mode": "off"})
 db.connect()
